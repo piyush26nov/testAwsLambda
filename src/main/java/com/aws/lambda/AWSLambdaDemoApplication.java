b7d8b07c76560bb08fadc2cb3e01f99c265e3460
Piyush <piyush26nov@gmail.com>
2022-08-24 01:03:47 +0530

    Initial Draft

---
+++ src/main/java/com/aws/lambda/AWSLambdaDemoApplication.java
@@ -0,0 +1,11 @@
+package com.aws.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWSLambdaDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AWSLambdaDemoApplication.class, args);
+    }
+}
